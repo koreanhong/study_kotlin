f3a87987ccbd7604867ace9ccd0a25e36bfcd8a1
hkhong <hkhong@enliple.com>
2024-08-07 12:56:39 +0900

    first commit

---
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -0,0 +1,33 @@
+package com.example.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoApplication>(*args)
+}
+
+@RestController
+class MessageController {
+    @GetMapping("/")
+    fun index() = listOf(
+            Message("1", "Hello!"),
+            Message("2", "Bonjour!"),
+            Message("3", "Privet!"),
+    )
+}
+
+
+
+data class Message(val id: String?, val text: String)
+

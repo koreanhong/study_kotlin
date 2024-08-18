ed634c85e14968249a05ea895e8fb5f3b4bc4dc7
hkhong <hyeonki1994@gmail.com>
2024-08-18 21:54:24 +0900

    feat: api 수정

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
+import org.springframework.jdbc.core.query
 
 @SpringBootApplication
 class DemoApplication
@@ -23,6 +24,9 @@ class MessageController(val service: MessageService) {
     @GetMapping("/")
     fun index(): List<Message> = service.findMessages()
 
+    @GetMapping("/{id}")
+    fun index(@PathVariable id: String): List<Message> =
+        service.findMessageById(id)
     @PostMapping("/")
     fun post(@RequestBody message: Message) {
         service.save(message)
@@ -37,11 +41,15 @@ class MessageService(val db: JdbcTemplate) {
         Message(response.getString("id"), response.getString("text"))
     }
 
+    fun findMessageById(id: String): List<Message> = db.query("select * from messages where id = ?", id) { response, _ ->
+        Message(response.getString("id"), response.getString("text"))
+    }
+
     fun save(message: Message) {
         val id = message.id ?: UUID.randomUUID().toString()
         db.update(
-                "insert into messages values ( ?, ? )",
-                id, message.text
+            "insert into messages values ( ?, ? )",
+            id, message.text
         )
     }
 }
